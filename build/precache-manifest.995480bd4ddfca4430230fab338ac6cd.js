9727e88e47f1ec0a950c53c7a377e2256e25f1fe
sufi Noor <sufibinnoor@hotmail.com>
2019-07-18 11:23:34 -0400

    adding all the files with React app and gCloud Build

---
+++ build/precache-manifest.995480bd4ddfca4430230fab338ac6cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98115167878011d696a1d6c4dc6c991b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80a72eb75c6c59a3d193",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "80a72eb75c6c59a3d193",
+    "url": "/static/js/main.9b798c00.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
